60d34e7603b6f0c501b65d615b93db545f248ee8
Shah Shalin <shalinshh723@gmail.com>
2024-10-25 13:04:41 +0530

    refactor: Updated twentythirteen theme's twentythirteen-block-style version

--- src/wp-content/themes/twentythirteen/functions.php
+++ src/wp-content/themes/twentythirteen/functions.php
@@ -343,7 +343,7 @@ function twentythirteen_scripts_styles() {
 	wp_enqueue_style( 'twentythirteen-style', get_stylesheet_uri(), array(), '20241112' );
 
 	// Theme block stylesheet.
-	wp_enqueue_style( 'twentythirteen-block-style', get_template_directory_uri() . '/css/blocks.css', array( 'twentythirteen-style' ), '20231016' );
+	wp_enqueue_style( 'twentythirteen-block-style', get_template_directory_uri() . '/css/blocks.css', array( 'twentythirteen-style' ), '20240520' );
 
 	// Registers the Internet Explorer specific stylesheet.
 	wp_register_style( 'twentythirteen-ie', get_template_directory_uri() . '/css/ie.css', array( 'twentythirteen-style' ), '20150214' );
