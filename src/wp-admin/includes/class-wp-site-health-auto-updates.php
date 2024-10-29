5513d245e7615373a7044af19df3b7100136ee56
Dovid Levine <david@axepress.dev>
2024-10-29 15:34:04 +0200

    chore: Avoid capitalizing `null` type in doc-blocks.

--- src/wp-admin/includes/class-wp-site-health-auto-updates.php
+++ src/wp-admin/includes/class-wp-site-health-auto-updates.php
@@ -66,7 +66,7 @@ class WP_Site_Health_Auto_Updates {
 	 * @param string $constant         The name of the constant to check.
 	 * @param bool|string|array $value The value that the constant should be, if set,
 	 *                                 or an array of acceptable values.
-	 * @return ?array The test results. Null if the test passed.
+	 * @return ?array The test results, or null if the test passed.
 	 */
 	public function test_constants( $constant, $value ) {
 		$acceptable_values = (array) $value;
@@ -91,7 +91,7 @@ class WP_Site_Health_Auto_Updates {
 	 *
 	 * @since 5.2.0
 	 *
-	 * @return ?array The test results. Null if the test passed.
+	 * @return ?array The test results or null if the test passed.
 	 */
 	public function test_wp_version_check_attached() {
 		if ( ( ! is_multisite() || is_main_site() && is_network_admin() )
@@ -115,7 +115,7 @@ class WP_Site_Health_Auto_Updates {
 	 *
 	 * @since 5.2.0
 	 *
-	 * @return ?array The test results. Null if the test passed.
+	 * @return ?array The test results or null if the test passed.
 	 */
 	public function test_filters_automatic_updater_disabled() {
 		/** This filter is documented in wp-admin/includes/class-wp-automatic-updater.php */
@@ -403,7 +403,7 @@ class WP_Site_Health_Auto_Updates {
 	 *
 	 * @since 5.2.0
 	 *
-	 * @return array|false|null The test results. False if it isn't a development version. Null if the test passed.
+	 * @return array|false|null The test results. False if it isn't a development version. Returns null if the test passed.
 	 */
 	public function test_accepts_dev_updates() {
 		require ABSPATH . WPINC . '/version.php'; // $wp_version; // x.y.z
