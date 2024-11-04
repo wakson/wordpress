2bd78d5fb96c94f4a4953e69b38fa5b4a0a699aa
pavanpatil1 <pavan.patil@rtcamp.com>
2024-11-04 16:50:39 +0530

    Add e2e test for validate media deletion

---
+++ tests/e2e/specs/media-library/delete-media.test.js
@@ -0,0 +1,77 @@
+/**
+ * WordPress dependencies
+ */
+import { test, expect } from '@wordpress/e2e-test-utils-playwright';
+import path from 'path';
+
+test.describe( 'Delete Media', () => {
+	test.beforeAll( async ( { requestUtils } ) => {
+		const files = [
+			path.resolve( process.cwd(), 'assets/test_data_image1.jpeg' ),
+			path.resolve( process.cwd(), 'uploads/test_data_image2.jpeg' ),
+			path.resolve( process.cwd(), 'uploads/test_data_image3.jpeg' ),
+		];
+		for ( const file of files ) {
+			await requestUtils.uploadMedia( file );
+		}
+	} );
+
+	test.afterAll( async ( { requestUtils } ) => {
+		await requestUtils.deleteAllMedia();
+	} );
+
+	test( 'delete single media', async ( { page, admin } ) => {
+		await admin.visitAdminPage( 'upload.php?mode=list' );
+
+		// Hover on the first media.
+		await page
+			.locator(
+				'tr td.title.column-title.has-row-actions.column-primary'
+			)
+			.first()
+			.hover();
+		page.once( 'dialog', ( dialog ) => {
+			dialog
+				.accept()
+				.catch( ( err ) =>
+					console.error( 'Dialog accept failed:', err )
+				);
+		} );
+		await page
+			.locator( "tr[id^='post-'] a[aria-label^='Delete']" )
+			.first()
+			.click();
+
+		await expect(
+			page.locator( '#message p' ),
+			'Media got deleted successfully'
+		).toBeVisible();
+	} );
+
+	test( 'delete Bulk media', async ( { page, admin } ) => {
+		await admin.visitAdminPage( 'upload.php?mode=list' );
+
+		// Select the multiple media from the list.
+		await page.locator( 'input[name="media[]"]' ).first().click();
+		await page.locator( 'input[name="media[]"]' ).nth( 1 ).click();
+
+		await page
+			.locator( '#bulk-action-selector-top' )
+			.selectOption( 'delete' );
+
+		page.once( 'dialog', ( dialog ) => {
+			dialog
+				.accept()
+				.catch( ( err ) =>
+					console.error( 'Dialog accept failed:', err )
+				);
+		} );
+
+		await page.getByRole( 'button', { name: 'Apply' } ).first().click();
+
+		await expect(
+			page.locator( '#message p' ),
+			'Media got deleted successfully'
+		).toBeVisible();
+	} );
+} );
