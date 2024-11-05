d1df054fd15a69a72976743e65ca44b7b2aa57b8
pavanpatil1 <pavan.patil@rtcamp.com>
2024-11-05 23:06:09 +0530

    add test outside the folder

--- tests/e2e/specs/01_delete-media.test.js
+++ tests/e2e/specs/01_delete-media.test.js
@@ -4,19 +4,39 @@
 import { test, expect } from '@wordpress/e2e-test-utils-playwright';
 
 test.describe( 'Delete User', () => {
+	test.beforeAll( async ( { requestUtils } ) => {
+		function generateRandomUser( index ) {
+			return {
+				username: `testuser${ index }_${ Math.floor(
+					Math.random() * 10000
+				) }`,
+				email: `test${ index }_${ Math.floor(
+					Math.random() * 10000
+				) }@gmail.com`,
+				password: `admin${ index }`,
+				roles: 'subscriber',
+			};
+		}
+		const users = Array.from( { length: 2 }, ( _, index ) =>
+			generateRandomUser( index )
+		);
 
-	test( 'Delete Bulk Users', async ( { page, admin,requestUtils } ) => {
-        requestUtils.createUser( {
-			username: `test${Math.floor(Math.random() * 100000)}`,
-			email: `test${Math.floor(Math.random() * 100000)}@gmail.com`,
-			password: 'admin',
-			roles: 'subscriber',
+		users.forEach( ( user ) => {
+			requestUtils.createUser( user );
 		} );
-		await admin.visitAdminPage( '/users.php' );
-        console.log('Session Cookies:', await page.context().cookies());
+	} );
 
-		await page.getByRole( 'link', { name: 'Subscriber (1)' } ).click();
-		await page.getByLabel( 'Select test' ).check();
+	test( 'Delete Bulk Users', async ( { page, admin } ) => {
+		await admin.visitAdminPage( '/users.php' );
+
+		await page.locator( "a[href='users.php?role=subscriber']" ).click();
+		await page
+			.locator( 'th.check-column input[type="checkbox"]' )
+			.first()
+			.check();
+		await page
+			.locator( 'th.check-column input[type="checkbox"] >> nth=1' )
+			.check();
 		await page
 			.locator( '#bulk-action-selector-top' )
 			.selectOption( 'delete' );
@@ -24,6 +44,8 @@ test.describe( 'Delete User', () => {
 		await page.getByRole( 'button', { name: 'Confirm Deletion' } ).click();
 
 		// Expect successful user deletion
-		await expect( page.locator( '#message > p' ) ).toHaveText( /deleted./ );
+		await expect( page.locator( '#message > p' ) ).toHaveText(
+			/2 users deleted/
+		);
 	} );
 } );
