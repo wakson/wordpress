9ea8024891bd79a3eea878ca1beda321f3cb848d
pavanpatil1 <pavan.patil@rtcamp.com>
2024-11-05 12:51:56 +0530

    change asseration

--- tests/e2e/specs/media-library/delete-media.test.js
+++ tests/e2e/specs/media-library/delete-media.test.js
@@ -48,9 +48,8 @@ test.describe( 'Delete Media', () => {
 			.first()
 			.click();
         
-        await page.waitForSelector('#message')
-        const deletionMessage = page.locator('#message p');
-        await expect(deletionMessage).toBeVisible();
+        const deletionMessage = page.getByText('Media file permanently deleted.');
+		await expect(deletionMessage).toBeVisible();
 	} );
 
 	test( 'delete Bulk media', async ( { page, admin } ) => {
@@ -73,7 +72,7 @@ test.describe( 'Delete Media', () => {
 
 		await page.getByRole( 'button', { name: 'Apply' } ).first().click();
         await page.waitForSelector('#message')
-        const deletionMessage = page.locator('#message p');
-        await expect(deletionMessage).toBeVisible();
+        const deletionMessage = page.getByText('2 media files permanently deleted.');
+		await expect(deletionMessage).toBeVisible();
 	} );
 } );
