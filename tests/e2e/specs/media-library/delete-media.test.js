9491857439d41d905de8940408251e1293ce28aa
pavanpatil1 <pavan.patil@rtcamp.com>
2024-11-05 17:42:53 +0530

    fix asseration

--- tests/e2e/specs/media-library/delete-media.test.js
+++ tests/e2e/specs/media-library/delete-media.test.js
@@ -30,12 +30,12 @@ test.describe( 'Delete Media', () => {
 
 	test( 'delete single media', async ( { page, admin } ) => {
 		// Hover on the first media.
-		await page
-			.locator(
-				'tr td.title.column-title.has-row-actions.column-primary'
-			)
-			.first()
-			.hover();
+		await page.locator( 'input[name="media[]"]' ).first().click();
+
+        await page
+			.locator( '#bulk-action-selector-top' )
+			.selectOption( 'delete' );
+
 		page.once( 'dialog', ( dialog ) => {
 			dialog
 				.accept()
@@ -43,10 +43,8 @@ test.describe( 'Delete Media', () => {
 					console.error( 'Dialog accept failed:', err )
 				);
 		} );
-		await page
-			.locator( "tr[id^='post-'] a[aria-label^='Delete']" )
-			.first()
-			.click();
+
+		await page.getByRole( 'button', { name: 'Apply' } ).first().click();
         
         const deletionMessage = page.locator('#message p');
         await expect(deletionMessage).toContainText('permanently deleted');
