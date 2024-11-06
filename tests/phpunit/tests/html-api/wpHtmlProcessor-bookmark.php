f7af6e31073c56146544fc1ed8b8ab184a0a6eb7
Jon Surrell <sirreal@users.noreply.github.com>
2024-11-06 13:19:58 +0100

    Add test seeking from SVG namespace

--- tests/phpunit/tests/html-api/wpHtmlProcessor-bookmark.php
+++ tests/phpunit/tests/html-api/wpHtmlProcessor-bookmark.php
@@ -76,4 +76,94 @@ class Tests_HtmlApi_WpHtmlProcessor_Bookmark extends WP_UnitTestCase {
 		$this->assertSame( 'DIV', $processor->get_tag() );
 		$this->assertTrue( $processor->get_attribute( 'two' ) );
 	}
+
+	/**
+	 * Seeking into and out of foreign content requires updating the parsing namespace correctly.
+	 *
+	 * @ticket 62290
+	 */
+	public function test_full_processor_seek_back_from_foreign_content() {
+		$processor = WP_HTML_Processor::create_full_parser( '<html><body><custom-element /><svg><rect />' );
+		$this->assertTrue( $processor->next_tag( 'CUSTOM-ELEMENT' ) );
+		$this->assertTrue( $processor->set_bookmark( 'mark' ), 'Failed to set bookmark "mark".' );
+		$this->assertTrue( $processor->has_bookmark( 'mark' ), 'Failed "mark" has_bookmark check.' );
+
+		/*
+		 * <custom-element /> has self-closing flag, but HTML elements (that are not void elements) cannot self-close,
+		 * they must be closed by some means, usually a closing tag.
+		 *
+		 * If the div were interpreted as foreign content, it would self-close.
+		 */
+		$this->assertTrue( $processor->has_self_closing_flag() );
+		$this->assertTrue( $processor->expects_closer(), 'Incorrectly interpreted custom-element with self-closing flag as self-closing element.' );
+
+		// Proceed into foreign content.
+		$this->assertTrue( $processor->next_tag( 'RECT' ) );
+		$this->assertSame( 'svg', $processor->get_namespace() );
+		$this->assertTrue( $processor->has_self_closing_flag() );
+		$this->assertFalse( $processor->expects_closer() );
+		$this->assertSame( array( 'HTML', 'BODY', 'CUSTOM-ELEMENT', 'SVG', 'RECT' ), $processor->get_breadcrumbs() );
+
+		// Seek back.
+		$this->assertTrue( $processor->seek( 'mark' ), 'Failed to seek to bookmark "mark".' );
+		$this->assertSame( 'CUSTOM-ELEMENT', $processor->get_tag() );
+		// If the parsing namespace were not correct here (html),
+		// then the self-closing flag would be misinterpreted.
+		$this->assertTrue( $processor->has_self_closing_flag() );
+		$this->assertTrue( $processor->expects_closer(), 'Incorrectly interpreted custom-element with self-closing flag as self-closing element.' );
+
+		// Proceed into foreign content again.
+		$this->assertTrue( $processor->next_tag( 'RECT' ) );
+		$this->assertSame( 'svg', $processor->get_namespace() );
+		$this->assertTrue( $processor->has_self_closing_flag() );
+		$this->assertFalse( $processor->expects_closer() );
+
+		// The RECT should still descend from the CUSTOM-ELEMENT despite its self-closing flag.
+		$this->assertSame( array( 'HTML', 'BODY', 'CUSTOM-ELEMENT', 'SVG', 'RECT' ), $processor->get_breadcrumbs() );
+	}
+
+	/**
+	 * Seeking into and out of foreign content requires updating the parsing namespace correctly.
+	 *
+	 * @ticket 62290
+	 */
+	public function test_pragment_processor_seek_back_from_foreign_content() {
+		$processor = WP_HTML_Processor::create_fragment( '<custom-element /><svg><rect />' );
+		$this->assertTrue( $processor->next_tag( 'CUSTOM-ELEMENT' ) );
+		$this->assertTrue( $processor->set_bookmark( 'mark' ), 'Failed to set bookmark "mark".' );
+		$this->assertTrue( $processor->has_bookmark( 'mark' ), 'Failed "mark" has_bookmark check.' );
+
+		/*
+		 * <custom-element /> has self-closing flag, but HTML elements (that are not void elements) cannot self-close,
+		 * they must be closed by some means, usually a closing tag.
+		 *
+		 * If the div were interpreted as foreign content, it would self-close.
+		 */
+		$this->assertTrue( $processor->has_self_closing_flag() );
+		$this->assertTrue( $processor->expects_closer(), 'Incorrectly interpreted custom-element with self-closing flag as self-closing element.' );
+
+		// Proceed into foreign content.
+		$this->assertTrue( $processor->next_tag( 'RECT' ) );
+		$this->assertSame( 'svg', $processor->get_namespace() );
+		$this->assertTrue( $processor->has_self_closing_flag() );
+		$this->assertFalse( $processor->expects_closer() );
+		$this->assertSame( array( 'HTML', 'BODY', 'CUSTOM-ELEMENT', 'SVG', 'RECT' ), $processor->get_breadcrumbs() );
+
+		// Seek back.
+		$this->assertTrue( $processor->seek( 'mark' ), 'Failed to seek to bookmark "mark".' );
+		$this->assertSame( 'CUSTOM-ELEMENT', $processor->get_tag() );
+		// If the parsing namespace were not correct here (html),
+		// then the self-closing flag would be misinterpreted.
+		$this->assertTrue( $processor->has_self_closing_flag() );
+		$this->assertTrue( $processor->expects_closer(), 'Incorrectly interpreted custom-element with self-closing flag as self-closing element.' );
+
+		// Proceed into foreign content again.
+		$this->assertTrue( $processor->next_tag( 'RECT' ) );
+		$this->assertSame( 'svg', $processor->get_namespace() );
+		$this->assertTrue( $processor->has_self_closing_flag() );
+		$this->assertFalse( $processor->expects_closer() );
+
+		// The RECT should still descend from the CUSTOM-ELEMENT despite its self-closing flag.
+		$this->assertSame( array( 'HTML', 'BODY', 'CUSTOM-ELEMENT', 'SVG', 'RECT' ), $processor->get_breadcrumbs() );
+	}
 }
