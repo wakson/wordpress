2a420cdba079e39da35a1eccc353ab3dbd1149b4
Jon Surrell <sirreal@users.noreply.github.com>
2024-11-08 17:57:17 +0100

    Add special case for array "length" property

--- src/wp-includes/interactivity-api/class-wp-interactivity-api.php
+++ src/wp-includes/interactivity-api/class-wp-interactivity-api.php
@@ -579,6 +579,12 @@ final class WP_Interactivity_API {
 		$path_segments = explode( '.', $path );
 		$current       = $store;
 		foreach ( $path_segments as $path_segment ) {
+			// Special case for numeric arrays to add a length property like JavaScript arrays.
+			if ( 'length' === $path_segment && is_array( $current ) && array_is_list( $current ) ) {
+				$current = count( $current );
+				break;
+			}
+
 			if ( ( is_array( $current ) || $current instanceof ArrayAccess ) && isset( $current[ $path_segment ] ) ) {
 				$current = $current[ $path_segment ];
 			} elseif ( is_object( $current ) && isset( $current->$path_segment ) ) {
