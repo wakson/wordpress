c7a4c64895d5adcce300f8882878cc9d382c5afc
Mukesh Panchal <mukeshpanchal27@users.noreply.github.com>
2024-11-10 09:15:39 +0530

    Update regex

--- src/wp-admin/includes/image.php
+++ src/wp-admin/includes/image.php
@@ -351,7 +351,7 @@ function wp_create_image_subsizes( $file, $attachment_id ) {
 					 * be added and subsequently removed.
 					 */
 					$converted_file_name = $editor->generate_filename( 'converted' );
-					$converted_file_name = preg_replace( '/-converted\.[a-z0-9]+$/i', '.', $converted_file_name );
+					$converted_file_name = preg_replace( '/(-converted\.)([a-z0-9]+)$/i', '.$2', $converted_file_name );
 					$saved = $editor->save( $converted_file_name );
 				} else {
 					$saved = $editor->save();
