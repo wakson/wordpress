f8db7f5b0a344162e19c9168f7eb5074ff68ef5e
Peter Wilson <git-commit-by@peterwilson.cc>
2024-11-13 07:42:51 +1100

    Post WordPress 6.7 version bump.

--- src/wp-includes/version.php
+++ src/wp-includes/version.php
@@ -16,7 +16,7 @@
  *
  * @global string $wp_version
  */
-$wp_version = '6.7-src';
+$wp_version = '6.7.1-alpha-59394-src';
 
 /**
  * Holds the WordPress DB revision, increments when changes are made to the WordPress DB schema.
