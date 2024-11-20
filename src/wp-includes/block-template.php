158d3b60388a47f3a2b48355df9e607ea35e3988
Sarah Norris <1645628+mikachan@users.noreply.github.com>
2024-11-20 09:58:20 +0000

    Update src/wp-includes/block-template.php
    Co-authored-by: Dave Smith <getdavemail@gmail.com>

--- src/wp-includes/block-template.php
+++ src/wp-includes/block-template.php
@@ -36,7 +36,7 @@ function wp_render_empty_block_template_warning( $block_template ) {
 				%4$s
 			</a>
 		</div>',
-		$block_template->title,
+		esc_html( $block_template->title ),
 		__( 'This page is blank because the template is empty. You can reset or customize it in the Site Editor.' ),
 		get_edit_post_link( $block_template->wp_id, 'site-editor' ),
 		__( 'Edit template' )
