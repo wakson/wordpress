16b5f7eefaca81842e39152f107e6e71b3337033
Anton Vlasenko <vlasenko.anton@gmail.com>
2024-11-21 22:26:08 +0100

    Fix CS.

--- src/wp-includes/rest-api/endpoints/class-wp-rest-widgets-controller.php
+++ src/wp-includes/rest-api/endpoints/class-wp-rest-widgets-controller.php
@@ -690,8 +690,7 @@ class WP_REST_Widgets_Controller extends WP_REST_Controller {
 			'instance'      => null,
 		);
 
-		if ( 
-			'wp_inactive_widgets' !== $sidebar_id &&
+		if ( 'wp_inactive_widgets' !== $sidebar_id &&
 			rest_is_field_included( 'rendered', $fields )
 		) {
 			$prepared['rendered'] = trim( wp_render_widget( $widget_id, $sidebar_id ) );
