01282ad70f24439c8d92ef8b6c590f989010a124
Sören Wünsch <soeren.wrede@parshipgroup.com>
2024-11-26 17:02:11 +0100

    Format code

--- src/wp-includes/compat.php
+++ src/wp-includes/compat.php
@@ -557,7 +557,7 @@ if ( ! function_exists( 'array_find' ) ) {
 	 *
 	 * @since 6.8.0
 	 *
-	 * @param array $array       The array to search.
+	 * @param array    $array    The array to search.
 	 * @param callable $callback The callback to run for each element.
 	 * @return mixed|null The first element in the array that passes the `$callback`, otherwise null.
 	 */
@@ -580,7 +580,7 @@ if ( ! function_exists( 'array_find_key' ) ) {
 	 *
 	 * @since 6.8.0
 	 *
-	 * @param array $array       The array to search.
+	 * @param array    $array    The array to search.
 	 * @param callable $callback The callback to run for each element.
 	 * @return int|string|null The first key in the array that passes the `$callback`, otherwise null.
 	 */
@@ -603,7 +603,7 @@ if ( ! function_exists( 'array_any' ) ) {
 	 *
 	 * @since 6.8.0
 	 *
-	 * @param array $array       The array to check.
+	 * @param array    $array    The array to check.
 	 * @param callable $callback The callback to run for each element.
 	 * @return bool True if any element in the array passes the `$callback`, otherwise false.
 	 */
@@ -626,7 +626,7 @@ if ( ! function_exists( 'array_all' ) ) {
 	 *
 	 * @since 6.8.0
 	 *
-	 * @param array $array       The array to check.
+	 * @param array    $array    The array to check.
 	 * @param callable $callback The callback to run for each element.
 	 * @return bool True if all elements in the array pass the `$callback`, otherwise false.
 	 */
