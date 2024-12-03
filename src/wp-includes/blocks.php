a5cb5985e22cce642ae8e38aaa5dd0855f3bdac8
Bernie Reiter <ockham@raz.or.at>
2024-12-03 14:14:03 +0100

    Add since PHPDoc

--- src/wp-includes/blocks.php
+++ src/wp-includes/blocks.php
@@ -1276,6 +1276,7 @@ function insert_hooked_blocks_and_set_ignored_hooked_blocks_metadata( &$parsed_a
  * Hooks into the REST API response for the Posts endpoint and adds the first and last inner blocks.
  *
  * @since 6.6.0
+ * @since 6.8.0 Support non-`wp_navigation` post types.
  *
  * @param WP_REST_Response $response The response object.
  * @param WP_Post          $post     Post object.
