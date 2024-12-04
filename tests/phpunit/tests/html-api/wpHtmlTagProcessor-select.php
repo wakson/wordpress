784b2d913cbf469a3847b93a46c9c202f19091b7
Jon Surrell <sirreal@users.noreply.github.com>
2024-12-04 21:41:25 +0100

    Add a lot of selector integration tests

--- tests/phpunit/tests/html-api/wpHtmlTagProcessor-select.php
+++ tests/phpunit/tests/html-api/wpHtmlTagProcessor-select.php
@@ -39,16 +39,42 @@ class Tests_HtmlApi_WpHtmlTagProcessor_Select extends WP_UnitTestCase {
 	 */
 	public static function data_selectors(): array {
 		return array(
-			'simple type'            => array( '<div match>', 'div' ),
-			'any type'               => array( '<html match>', '*' ),
-			'simple class'           => array( '<div class="x" match>', '.x' ),
-			'simple id'              => array( '<div id="x" match>', '#x' ),
-			'simple attribute'       => array( '<div att match>', '[att]' ),
-			'attribute value'        => array( '<div att="val" match>', '[att=val]' ),
-			'attribute quoted value' => array( '<div att="::" match>', '[att="::"]' ),
+			'simple type'                           => array( '<p><div match>', 'div' ),
+			'any type'                              => array( '<div match>', '*' ),
+			'simple class'                          => array( '<div><div class="x" match>', '.x' ),
+			'simple id'                             => array( '<div><div id="x" match>', '#x' ),
+			'boolean attribute'                     => array( '<div tta><div att match>', '[att]' ),
+			'boolean attribute with string match'   => array( '<div tta><div att match>', '[att=""]' ),
 
-			'list'                   => array( '<div><p match>', 'a, p' ),
-			'compound'               => array( '<div att><section att="foo bar" match>', 'section[att~="bar"]' ),
+			'attribute value'                       => array( '<div att="v"><div att="val" match>', '[att=val]' ),
+			'attribute quoted value'                => array( '<div att><div att="::" match>', '[att="::"]' ),
+			'attribute case insensitive'            => array( '<div att="value"><div att="val" match>', '[att="VAL"i]' ),
+			'attribute case sensitive mod'          => array( '<div att="VAL"><div att="val" match>', '[att="val"s]' ),
+
+			'attribute one of'                      => array( '<div att="a B c"><div att="a b c" match>', '[att~="b"]' ),
+			'attribute one of insensitive'          => array( '<div att="a c"><div att="a B c" match>', '[att~="b"i]' ),
+			'attribute one of mod sensitive'        => array( '<div att="a B c"><div att="a b c" match>', '[att~="b"s]' ),
+			'attribute one of whitespace cases'     => array( "<div att='     a'><div att='\na\ta   b   ' match>", '[att~="b"]' ),
+
+			'attribute with-hyphen (no hyphen)'     => array( '<p att="special_not"><p att="special" match>', '[att|="special"]' ),
+			'attribute with-hyphen (hyphen prefix)' => array( '<p att="special_not"><p att="special-yeah" match>', '[att|="special"]' ),
+			'attribute with-hyphen insensitive'     => array( '<p att="special_not"><p att="SPECIAL" match>', '[att|="special"i]' ),
+			'attribute with-hyphen sensitive mod'   => array( '<p att="spec"><p att="special" match>', '[att|="special"s]' ),
+
+			'attribute prefixed'                    => array( '<p att="fix"><p att="prefix" match>', '[att^="p"]' ),
+			'attribute prefixed insensitive'        => array( '<p att="fix"><p att="Prefix" match>', '[att^="p"i]' ),
+			'attribute prefixed sensitive mod'      => array( '<p att="Prefix"><p att="prefix" match>', '[att^="p"s]' ),
+
+			'attribute suffixed'                    => array( '<p att="suffix_"><p att="suffix" match>', '[att$="x"]' ),
+			'attribute suffixed insensitive'        => array( '<p att="suffix_"><p att="suffiX" match>', '[att$="x"i]' ),
+			'attribute suffixed sensitive mod'      => array( '<p att="suffiX"><p att="suffix" match>', '[att$="x"s]' ),
+
+			'attribute contains'                    => array( '<p att="abcyz"><p att="abcxyz" match>', '[att*="x"]' ),
+			'attribute contains insensitive'        => array( '<p att="abcyz"><p att="abcXyz" match>', '[att*="x"i]' ),
+			'attribute contains sensitive mod'      => array( '<p att="abcXyz"><p att="abcxyz" match>', '[att*="x"s]' ),
+
+			'list'                                  => array( '<div><p match>', 'a, p' ),
+			'compound'                              => array( '<div att><section att="bar" match>', 'section[att="bar"]' ),
 		);
 	}
 
