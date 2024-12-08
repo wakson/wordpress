93d6e51656c4d2d308e9c4fe2dff27964eda719b
tellthemachines <isabel@tellthemachines.com>
2024-12-09 10:45:10 +1100

    Update docblocks for `wp_get_global_stylesheet` and `WP_Theme_JSON::get_stylesheet`.

--- src/wp-includes/global-styles-and-settings.php
+++ src/wp-includes/global-styles-and-settings.php
@@ -142,7 +142,7 @@ function wp_get_global_styles( $path = array(), $context = array() ) {
  * @since 6.6.0 Resolves relative paths in theme.json styles to theme absolute paths.
  *
  * @param array $types Optional. Types of styles to load.
- *                     It accepts as values 'variables', 'presets', 'styles', 'base-layout-styles'.
+ *                     It accepts as values 'variables', 'presets', 'styles', 'base-layout-styles', 'custom-css'.
  *                     If empty, it'll load the following:
  *                     - for themes without theme.json: 'variables', 'presets', 'base-layout-styles'.
  *                     - for themes with theme.json: 'variables', 'presets', 'styles'.
