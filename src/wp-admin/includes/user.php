e47c210eba49373ed6c376723c2b04240c36677a
PRASAD KARMALKAR <prasadkarmalkar2020@gmail.com>
2024-12-16 17:13:38 +0530

    Remove condition from updating user value

--- src/wp-admin/includes/user.php
+++ src/wp-admin/includes/user.php
@@ -132,9 +132,7 @@ function edit_user( $user_id = 0 ) {
 	}
 
 	if ( $update ) {
-		if ( $user->rich_editing === 'false' ) {
-			$user->rich_editing = isset( $_POST['rich_editing'] ) && 'false' === $_POST['rich_editing'] ? 'false' : 'true';
-		}
+		$user->rich_editing         = isset( $_POST['rich_editing'] ) && 'false' === $_POST['rich_editing'] ? 'false' : 'true';
 		$user->syntax_highlighting  = isset( $_POST['syntax_highlighting'] ) && 'false' === $_POST['syntax_highlighting'] ? 'false' : 'true';
 		$user->admin_color          = isset( $_POST['admin_color'] ) ? sanitize_text_field( $_POST['admin_color'] ) : 'fresh';
 		$user->show_admin_bar_front = isset( $_POST['admin_bar_front'] ) ? 'true' : 'false';
