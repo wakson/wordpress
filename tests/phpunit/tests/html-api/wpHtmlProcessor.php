35bbd389344f831bc32e02f557628e289f6c1f5b
Jonathan Desrosiers <desrosj@git.wordpress.org>
2024-12-18 18:38:58 +0000

    HTML API: Use case insensitive tag_name comparison in `::next_tag`.
    The HTML API `::next_tag` method now performs case-insensitive matching when searching for tags by name. For example, searching for 'DIV' will match both '<div>' and '<DIV>' tags.
    
    Reviewed by desrosj.
    Merges [59422] to the 6.7 branch.
    
    Props jonsurrell, dmsnell, czapla.
    Fixes #62427.
    
    git-svn-id: https://develop.svn.wordpress.org/branches/6.7@59535 602fd350-edb4-49c9-b593-d223f7449a82

--- tests/phpunit/tests/html-api/wpHtmlProcessor.php
+++ tests/phpunit/tests/html-api/wpHtmlProcessor.php
@@ -882,4 +882,19 @@ class Tests_HtmlApi_WpHtmlProcessor extends WP_UnitTestCase {
 		$this->assertSame( 'FORM', $processor->get_tag() );
 		$this->assertTrue( $processor->is_tag_closer() );
 	}
+
+	/**
+	 * Ensure that lowercased tag_name query matches tags case-insensitively.
+	 *
+	 * @group 62427
+	 */
+	public function test_next_tag_lowercase_tag_name() {
+		// The upper case <DIV> is irrelevant but illustrates the case-insentivity.
+		$processor = WP_HTML_Processor::create_fragment( '<section><DIV>' );
+		$this->assertTrue( $processor->next_tag( array( 'tag_name' => 'div' ) ) );
+
+		// The upper case <RECT> is irrelevant but illustrates the case-insentivity.
+		$processor = WP_HTML_Processor::create_fragment( '<svg><RECT>' );
+		$this->assertTrue( $processor->next_tag( array( 'tag_name' => 'rect' ) ) );
+	}
 }
